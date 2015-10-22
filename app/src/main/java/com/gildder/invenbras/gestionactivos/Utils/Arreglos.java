334d8e62c157ced988acdab3d7bb9935355be04a
Gildder <gilberet@hotmail.com>
2015-10-22 09:38:45 -0400

    terminamos la toma de fotos y comenzamos la DB

---
+++ app/src/main/java/com/gildder/invenbras/gestionactivos/Utils/Arreglos.java
@@ -0,0 +1,11 @@
+package com.gildder.invenbras.gestionactivos.Utils;
+
+/**
+ * Created by gildder on 19/10/2015.
+ */
+public abstract class Arreglos {
+
+    public final static String[] ESTADO_ACTIVO = {"Sin estado", "Funcional", "No Funcional"};
+
+
+}
